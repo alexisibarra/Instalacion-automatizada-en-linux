d69025248203f3f2fd71037678ac2ca39ddff12e
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2015-01-08 14:26:23 -0430

    Nuevos archivos

--- aptitudeInstall.sh
+++ aptitudeInstall.sh
@@ -16,36 +16,36 @@ aptitude install calibre &&                             echo -e "$COL_GREEN cali
 aptitude install filezilla &&                           echo -e "$COL_GREEN filezilla instalada $COL_RESET" && 
 aptitude install firmware-realtek &&                    echo -e "$COL_GREEN firmware-realtek instalada $COL_RESET" && 
 aptitude install g++ &&                                 echo -e "$COL_GREEN g++ instalada $COL_RESET" && 
-aptitude install gimp &&                                echo -e "$COL_GREEN gimp instalada $COL_RESET" && 
+#aptitude install gimp &&                                echo -e "$COL_GREEN gimp instalada $COL_RESET" && 
 aptitude install gnome-do &&                            echo -e "$COL_GREEN gnome-do instalada $COL_RESET" && 
 aptitude install google-chrome-stable &&                echo -e "$COL_GREEN google-chrome-stable instalada $COL_RESET" && 
 aptitude install gparted &&                             echo -e "$COL_GREEN gparted instalada $COL_RESET" && 
 aptitude install guake &&                               echo -e "$COL_GREEN guake instalada $COL_RESET" && 
-aptitude install lyx &&                                 echo -e "$COL_GREEN lyx instalada $COL_RESET" && 
-aptitude install pandoc &&                              echo -e "$COL_GREEN pandoc instalada $COL_RESET" && 
-aptitude install phatch &&                              echo -e "$COL_GREEN phatch instalada $COL_RESET" && 
+#aptitude install lyx &&                                 echo -e "$COL_GREEN lyx instalada $COL_RESET" && 
+#aptitude install pandoc &&                              echo -e "$COL_GREEN pandoc instalada $COL_RESET" && 
+#aptitude install phatch &&                              echo -e "$COL_GREEN phatch instalada $COL_RESET" && 
 aptitude install podbrowser &&                          echo -e "$COL_GREEN podbrowser instalada $COL_RESET" && 
 aptitude install redshift &&                            echo -e "$COL_GREEN redshift instalada $COL_RESET" && 
 aptitude install rhythmbox &&                           echo -e "$COL_GREEN rhythmbox instalada $COL_RESET" && 
 aptitude install screen &&                              echo -e "$COL_GREEN screen instalada $COL_RESET" && 
 aptitude install teamviewer &&                          echo -e "$COL_GREEN teamviewer instalada $COL_RESET" && 
 aptitude install terminator &&                          echo -e "$COL_GREEN terminator instalada $COL_RESET" && 
-aptitude install texlive-latex-base &&                  echo -e "$COL_GREEN texlive-latex-base instalada $COL_RESET" && 
-aptitude install texlive-latex-extra &&                 echo -e "$COL_GREEN texlive-latex-extra instalada $COL_RESET" && 
+#aptitude install texlive-latex-base &&                  echo -e "$COL_GREEN texlive-latex-base instalada $COL_RESET" && 
+#aptitude install texlive-latex-extra &&                 echo -e "$COL_GREEN texlive-latex-extra instalada $COL_RESET" && 
 aptitude install transmission &&                        echo -e "$COL_GREEN transmission instalada $COL_RESET" && 
 aptitude install vim &&                                 echo -e "$COL_GREEN vim instalada $COL_RESET" && 
 aptitude install vim-gtk &&                             echo -e "$COL_GREEN vim-gtk instalada $COL_RESET" && 
 aptitude install virtualbox &&                          echo -e "$COL_GREEN virtualbox instalada $COL_RESET" && 
 aptitude install vlc &&                                 echo -e "$COL_GREEN vlc instalada $COL_RESET" && 
 aptitude install wicd-{client,gtk} &&                   echo -e "$COL_GREEN wicd-{client,gtk} instalada $COL_RESET" && 
-aptitude install xchm &&                                echo -e "$COL_GREEN xchm instalada $COL_RESET" && 
-aptitude install zim &&                                 echo -e "$COL_GREEN zim instalada $COL_RESET" && 
+#aptitude install xchm &&                                echo -e "$COL_GREEN xchm instalada $COL_RESET" && 
+#aptitude install zim &&                                 echo -e "$COL_GREEN zim instalada $COL_RESET" && 
 
 ### Development
 aptitude install dia &&                                 echo -e "$COL_GREEN dia instalada $COL_RESET" && 
 aptitude install exuberant-ctags &&                     echo -e "$COL_GREEN exuberant-ctags instalada $COL_RESET" && 
 aptitude install git{,g} &&                             echo -e "$COL_GREEN git instalada $COL_RESET" && 
-aptitude install openjdk-6-j{re,dk} &&                  echo -e "$COL_GREEN openjdk-6-j{re,dk} instalada $COL_RESET" && 
+#aptitude install openjdk-6-j{re,dk} &&                  echo -e "$COL_GREEN openjdk-6-j{re,dk} instalada $COL_RESET" && 
 aptitude install ruby &&                                echo -e "$COL_GREEN ruby instalada $COL_RESET" && 
 aptitude install ruby-mkrf &&                           echo -e "$COL_GREEN ruby-mkrf instalada $COL_RESET" && 
 
@@ -55,7 +55,7 @@ aptitude install libpq-dev &&                           echo -e "$COL_GREEN libp
 aptitude install libsqlite3-dev &&                      echo -e "$COL_GREEN Biblioteca sqlite3 instalada $COL_RESET" && 
 aptitude install mysql-{server,client} &&               echo -e "$COL_GREEN mysql-{server,client,admin,query-browser} instalada $COL_RESET" && 
 aptitude install php5-mysql &&                          echo -e "$COL_GREEN php5-mysql instalada $COL_RESET" && 
-aptitude install phpmyadmin &&                          echo -e "$COL_GREEN phpmyadmin instalada $COL_RESET" && 
+#aptitude install phpmyadmin &&                          echo -e "$COL_GREEN phpmyadmin instalada $COL_RESET" && 
 aptitude install sqlite3 &&                             echo -e "$COL_GREEN sqlite3 instalada $COL_RESET" && 
 
 ### Perl modules
---
+++ quicktile.sh
@@ -0,0 +1,3 @@
+git clone git://github.com/ssokolow/quicktile /home/alexis/bin
+sudo aptitude install python python-gtk2 python-xlib python-dbus python-wnck
+ln -s /home/alexis/dotfiles/quicktile.cfg /home/alexis/.config/
