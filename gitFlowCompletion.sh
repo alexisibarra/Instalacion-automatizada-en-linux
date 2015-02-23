5f4c421358465394b6e960002424fe06da6d37d4
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2015-02-23 10:29:00 -0430

    Añadiendo automatización para configuración git flow completion

---
+++ gitFlowCompletion.sh
@@ -0,0 +1,5 @@
+sudo apt-get install git-core bash-completion;
+touch /etc/bash_completion.d/git-flow-completion.bash;
+wget -O /etc/bash_completion.d/git-flow-completion.bash https://raw.githubusercontent.com/bobthecow/git-flow-completion/master/git-flow-completion.bash ;
+# wget -O ~/bin/git-flow-completion.bash https://raw.githubusercontent.com/bobthecow/git-flow-completion/master/git-flow-completion.bash ;
+source ~/.bashrc;
