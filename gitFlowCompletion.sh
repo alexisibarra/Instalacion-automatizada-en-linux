ef625194f203ae9d290ee6b8db2d422ef5b1a367
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2015-03-12 11:52:49 -0430

    Modificaciones minimas

--- gitFlowCompletion.sh
+++ gitFlowCompletion.sh
@@ -1,5 +1,4 @@
 sudo apt-get install git-core git-flow bash-completion;
 touch /etc/bash_completion.d/git-flow-completion.bash;
 wget -O /etc/bash_completion.d/git-flow-completion.bash https://raw.githubusercontent.com/bobthecow/git-flow-completion/master/git-flow-completion.bash ;
-# wget -O ~/bin/git-flow-completion.bash https://raw.githubusercontent.com/bobthecow/git-flow-completion/master/git-flow-completion.bash ;
 source ~/.bashrc;
