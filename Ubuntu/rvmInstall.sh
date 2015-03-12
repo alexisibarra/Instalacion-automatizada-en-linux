d75da0b3158249c8246fd0e224a71cb31c8c1e05
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2015-03-12 11:42:32 -0430

    rvm installation

---
+++ Ubuntu/rvmInstall.sh
@@ -0,0 +1,7 @@
+sudo apt-get update
+sudo apt-get install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev libgdbm-dev libncurses5-dev automake libtool bison libffi-dev curl
+
+curl -L https://get.rvm.io | bash -s
+
+source ~/.rvm/scripts/rvm
+
