1215180f1d93bdb7bcd98174f7991ba257426fd3
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2015-03-12 11:49:34 -0430

    Adding git-flow in the installation

--- gitFlowCompletion.sh
+++ gitFlowCompletion.sh
@@ -1,4 +1,4 @@
-sudo apt-get install git-core bash-completion;
+sudo apt-get install git-core git-flow bash-completion;
 touch /etc/bash_completion.d/git-flow-completion.bash;
 wget -O /etc/bash_completion.d/git-flow-completion.bash https://raw.githubusercontent.com/bobthecow/git-flow-completion/master/git-flow-completion.bash ;
 # wget -O ~/bin/git-flow-completion.bash https://raw.githubusercontent.com/bobthecow/git-flow-completion/master/git-flow-completion.bash ;
