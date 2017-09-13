440915ab4f7d9ce04f6e4f759a12f21abe74d69d
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2017-09-13 10:53:03 -0400

    forcing symbolic link

--- oh-my-zshInstall.sh
+++ oh-my-zshInstall.sh
@@ -1,3 +1,3 @@
 sudo apt-get install -y zsh
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-ln -s ~/dotfiles/.zshrc ~/
+ln -sf ~/dotfiles/.zshrc ~/
