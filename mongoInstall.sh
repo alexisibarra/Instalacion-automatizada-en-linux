78a4a4fbdb2d5886547e24fef13f3864ec4f5ffc
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2018-08-11 09:05:03 -0400

    add mongo install

---
+++ mongoInstall.sh
@@ -0,0 +1,4 @@
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org mongodb-org-server mongodb-org-shell mongodb-org-mongos mongodb-org-tools
