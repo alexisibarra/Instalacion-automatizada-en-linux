5a4cb3df41ea23e3d4ffdf1131a528bf49f01ba5
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2019-02-04 17:39:42 -0400

    add new way to install node

--- nodeInstall.sh
+++ nodeInstall.sh
@@ -1,14 +1,21 @@
 # #!/bin/bash
 
 ### Instalando Node
-mkdir .tmp &&
-wget http://nodejs.org/dist/v8.9.4/node-v8.9.4.tar.gz -P .tmp &&
-cd .tmp &&
-tar -xvzf node-v8.9.4.tar.gz &&
-rm node-v8.9.4.tar.gz &&
-cd node-v8.9.4 &&
-./configure &&
-make &&
-sudo make install &&
-cd ../../ &&
-rm -r .tmp
+# mkdir .tmp &&
+# wget http://nodejs.org/dist/v8.9.4/node-v8.9.4.tar.gz -P .tmp &&
+# cd .tmp &&
+# tar -xvzf node-v8.9.4.tar.gz &&
+# rm node-v8.9.4.tar.gz &&
+# cd node-v8.9.4 &&
+# ./configure &&
+# make &&
+# sudo make install &&
+# cd ../../ &&
+# rm -r .tmp
+
+
+# Adding the NodeSource APT repository for Debian-based distributions repository AND the PGP key for verifying packages
+curl -sL https://deb.nodesource.com/setup_11.x | sudo -E bash -
+
+# Install Node.js from the Debian-based distributions repository
+sudo apt-get install -y nodejs
